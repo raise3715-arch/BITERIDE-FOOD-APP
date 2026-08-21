3e4c63346f3457e0ea40698f69db6037fb372c9b
raise3715-arch <raise3715@gmail.com>
2026-08-21 19:43:44 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tiffin-co-v1";
+const CACHE_NAME = "nesh-v1";
 const APP_SHELL = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
